f6aae7ce39ffca13e6b50d2aa268b73cc4ee8dfe
Yanpeng Jia <75295024+Yaepiii@users.noreply.github.com>
2024-02-04 15:47:36 +0800

    Add files via upload

---
+++ gtsam_examples/带注释/LocalizationExample.cpp
@@ -0,0 +1,199 @@
+﻿/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file LocalizationExample.cpp
+ * @brief Simple robot localization example, with three "GPS-like" measurements
+ * @author Frank Dellaert
+ */
+
+/**
+ * A simple 2D pose slam example with "GPS" measurements
+ *  - The robot moves forward 2 meter each iteration
+ *  - The robot initially faces along the X axis (horizontal, to the right in 2D)
+ *  - We have full odometry between pose
+ *  - We have "GPS-like" measurements implemented with a custom factor
+ 一个带有“ GPS”测量值的简单2D位姿slam示例
+    -机器人每次迭代向前移动2米
+    -机器人最初的朝向为X轴正向（水平，在2D中为向右）
+    -位姿之间有完整的里程
+    -我们通过一个自定义因子实现了“类GPS”的测量
+ */
+
+// We will use Pose2 variables (x, y, theta) to represent the robot positions
+//我们将使用Pose2变量(x, y, theta)表示机器人位姿
+#include <gtsam/geometry/Pose2.h>
+
+// We will use simple integer Keys to refer to the robot poses.
+//我们将使用简单的整数key来代表机器人位姿。
+#include <gtsam/inference/Key.h>
+
+// As in OdometryExample.cpp, we use a BetweenFactor to model odometry measurements.
+//与OdometryExample.cpp中一样，我们使用BetweenFactor对里程计测量进行建模。
+#include <gtsam/slam/BetweenFactor.h>
+
+// We add all facors to a Nonlinear Factor Graph, as our factors are nonlinear.
+//我们将所有因子添加到非线性因子图中，因为我们的因子是非线性的。
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+
+// The nonlinear solvers within GTSAM are iterative solvers, meaning they linearize the
+// nonlinear functions around an initial linearization point, then solve the linear system
+// to update the linearization point. This happens repeatedly until the solver converges
+// to a consistent set of variable values. This requires us to specify an initial guess
+// for each variable, held in a Values container.
+/*
+GTSAM中的非线性求解器是迭代求解器，这意味着它们会在初始线性化点附近线性化非线性函数，
+然后求解线性系统以更新线性化点。这会反复发生，直到求解器收敛到一组一致的变量值为止。
+这要求我们为每个变量指定一个初始估计，将其保存在“值”容器中。
+*/
+#include <gtsam/nonlinear/Values.h>
+
+// Finally, once all of the factors have been added to our factor graph, we will want to
+// solve/optimize to graph to find the best (Maximum A Posteriori) set of variable values.
+// GTSAM includes several nonlinear optimizers to perform this step. Here we will use the
+// standard Levenberg-Marquardt solver
+/*
+最后，将所有因子添加到因子图中后，我们将需要求解/优化以找到最佳变量值集（最大后验）。
+GTSAM包含多个非线性优化器来执行此步骤。在这里，我们将使用标准的Levenberg-Marquardt求解器。
+*/
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+
+// Once the optimized values have been calculated, we can also calculate the marginal covariance
+// of desired variables
+//一旦计算出最佳值，我们也可以计算所需变量的边际协方差（边缘分布）
+#include <gtsam/nonlinear/Marginals.h>
+
+using namespace std;
+using namespace gtsam;
+
+// Before we begin the example, we must create a custom unary factor to implement a
+// "GPS-like" functionality. Because standard GPS measurements provide information
+// only on the position, and not on the orientation, we cannot use a simple prior to
+// properly model this measurement.
+//在开始示例之前，我们必须创建一个自定义一元因子以实现“类GPS”的功能。
+//由于标准GPS测量仅提供有关位置的信息，而不能提供有关方向的信息，
+//因此我们无法使用一个简单的先验来正确建模此测量。
+//
+// The factor will be a unary factor, affect only a single system variable. It will
+// also use a standard Gaussian noise model. Hence, we will derive our new factor from
+// the NoiseModelFactor1.
+//该因子将是一元因子，仅影响单个系统变量。它还将使用标准的高斯噪声模型。
+//因此，我们将从NoiseModelFactor1导出新因子。
+#include <gtsam/nonlinear/NonlinearFactor.h>
+
+class UnaryFactor: public NoiseModelFactor1<Pose2> { //NoiseModelFactor1为一元因子类
+
+  // The factor will hold a measurement consisting of an (X,Y) location
+  // We could this with a Point2 but here we just use two doubles
+  //该因子将进行包含(X,Y)位置的测量
+  //我们可以用Point2做到这一点，但在这里我们仅使用两个double型数据
+  double mx_, my_;
+
+public:
+  /// shorthand for a smart pointer to a factor
+  ///指向因子的智能指针的简写
+  typedef boost::shared_ptr<UnaryFactor> shared_ptr;
+
+  // The constructor requires the variable key, the (X, Y) measurement value, and the noise model
+  //构造函数需要变量的key，(X, Y)测量值和噪声模型
+  UnaryFactor(Key j, double x, double y, const SharedNoiseModel& model):
+    NoiseModelFactor1<Pose2>(model, j), mx_(x), my_(y) {} //NoiseModelFactor1<Pose2>(model, j)是带有变量key和噪声模型的基础类
+
+  virtual ~UnaryFactor() {}
+
+  // Using the NoiseModelFactor1 base class there are two functions that must be overridden.
+  // The first is the 'evaluateError' function. This function implements the desired measurement
+  // function, returning a vector of errors when evaluated at the provided variable value. It
+  // must also calculate the Jacobians for this measurement function, if requested.
+  //使用NoiseModelFactor1基类，必须重写两个函数。第一个是'evaluateError'函数。
+  //此函数实现所需的测量功能，当评估所提供变量值时返回误差向量。
+  //如果需要，它还必须计算此测量函数的雅可比矩阵。
+  //q是Pose2的指针
+  //H是可选的雅可比矩阵的指针，它使用了boost::optional并且默认为空指针
+  Vector evaluateError(const Pose2& q, boost::optional<Matrix&> H = boost::none) const
+  {
+    // The measurement function for a GPS-like measurement is simple:
+    // error_x = pose.x - measurement.x
+    // error_y = pose.y - measurement.y
+    // Consequently, the Jacobians are:
+    // [ derror_x/dx  derror_x/dy  derror_x/dtheta ] = [1 0 0]
+    // [ derror_y/dx  derror_y/dy  derror_y/dtheta ] = [0 1 0]
+	//Jacobians
+	// note that use boost optional like a pointer
+	// only calculate jacobian matrix when non-null pointer exists
+    if (H) (*H) = (Matrix(2,3) << 1.0,0.0,0.0, 0.0,1.0,0.0).finished();
+	// return error vector
+    return (Vector(2) << q.x() - mx_, q.y() - my_).finished();
+  }
+
+  // The second is a 'clone' function that allows the factor to be copied. Under most
+  // circumstances, the following code that employs the default copy constructor should
+  // work fine.
+  //第二个是'clone'函数，它允许因子被复制。在大多数情况下，采用默认副本构造函数的以下代码应该可以正常工作。
+  virtual gtsam::NonlinearFactor::shared_ptr clone() const {
+    return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+        gtsam::NonlinearFactor::shared_ptr(new UnaryFactor(*this))); }
+
+  // Additionally, we encourage you the use of unit testing your custom factors,
+  // (as all GTSAM factors are), in which you would need an equals and print, to satisfy the
+  // GTSAM_CONCEPT_TESTABLE_INST(T) defined in Testable.h, but these are not needed below.
+  //此外，我们建议您单独使用测试您的自定义因子（正如所有GTSAM因子一样），
+  //在过程中您需要等值并打印，以满足Testable.h中定义的GTSAM_CONCEPT_TESTABLE_INST(T)，但下面的部分不需要这些。
+
+}; // UnaryFactor
+
+
+int main(int argc, char** argv) {
+
+  // 1. Create a factor graph container and add factors to it
+  NonlinearFactorGraph graph;
+
+  // 2a. Add odometry factors
+  // For simplicity, we will use the same noise model for each odometry factor
+  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Sigmas(Vector3(0.2, 0.2, 0.1));
+  // Create odometry (Between) factors between consecutive poses
+  graph.emplace_shared<BetweenFactor<Pose2> >(1, 2, Pose2(2.0, 0.0, 0.0), odometryNoise);
+  graph.emplace_shared<BetweenFactor<Pose2> >(2, 3, Pose2(2.0, 0.0, 0.0), odometryNoise);
+
+  // 2b. Add "GPS-like" measurements
+  // We will use our custom UnaryFactor for this.
+  noiseModel::Diagonal::shared_ptr unaryNoise = noiseModel::Diagonal::Sigmas(Vector2(0.1, 0.1)); // 10cm std on x,y
+  graph.emplace_shared<UnaryFactor>(1, 0.0, 0.0, unaryNoise);
+  graph.emplace_shared<UnaryFactor>(2, 2.0, 0.0, unaryNoise);
+  graph.emplace_shared<UnaryFactor>(3, 4.0, 0.0, unaryNoise);
+  graph.print("\nFactor Graph:\n"); // print
+
+  // 3. Create the data structure to hold the initialEstimate estimate to the solution
+  // For illustrative purposes, these have been deliberately set to incorrect values
+  Values initialEstimate;
+  initialEstimate.insert(1, Pose2(0.5, 0.0, 0.2));
+  initialEstimate.insert(2, Pose2(2.3, 0.1, -0.2));
+  initialEstimate.insert(3, Pose2(4.1, 0.1, 0.1));
+  initialEstimate.print("\nInitial Estimate:\n"); // print
+
+  // 4. Optimize using Levenberg-Marquardt optimization. The optimizer
+  // accepts an optional set of configuration parameters, controlling
+  // things like convergence criteria, the type of linear system solver
+  // to use, and the amount of information displayed during optimization.
+  // Here we will use the default set of parameters.  See the
+  // documentation for the full set of parameters.
+  LevenbergMarquardtOptimizer optimizer(graph, initialEstimate);
+  Values result = optimizer.optimize();
+  result.print("Final Result:\n");
+
+  // 5. Calculate and print marginal covariances for all variables
+  Marginals marginals(graph, result);
+  cout << "x1 covariance:\n" << marginals.marginalCovariance(1) << endl;
+  cout << "x2 covariance:\n" << marginals.marginalCovariance(2) << endl;
+  cout << "x3 covariance:\n" << marginals.marginalCovariance(3) << endl;
+
+  return 0;
+}
---
+++ gtsam_examples/带注释/SimpleRotation.cpp
@@ -0,0 +1,159 @@
+﻿/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file SimpleRotation.cpp
+ * @brief This is a super-simple example of optimizing a single rotation according to a single prior
+ * @date Jul 1, 2010
+ * @author Frank Dellaert
+ * @author Alex Cunningham
+ */
+
+  /**
+   * This example will perform a relatively trivial optimization on
+   * a single variable with a single factor.
+   *此示例将对具有单个因子的单个变量执行相对轻微的优化。
+   */
+
+// In this example, a 2D rotation will be used as the variable of interest
+//在此示例中，将使用2D旋转作为目标变量
+#include <gtsam/geometry/Rot2.h>
+
+// Each variable in the system (poses) must be identified with a unique key.
+// We can either use simple integer keys (1, 2, 3, ...) or symbols (X1, X2, L1).
+// Here we will use symbols
+//系统中的每个变量（位姿）都必须使用唯一的key进行标识，我们可以使用简单的整数key(1, 2, 3, ...)或符号(X1, X2, L1).
+//此处将使用符号
+#include <gtsam/inference/Symbol.h>
+
+// In GTSAM, measurement functions are represented as 'factors'. Several common factors
+// have been provided with the library for solving robotics/SLAM/Bundle Adjustment problems.
+// We will apply a simple prior on the rotation. We do so via the `addPrior` convenience
+// method in NonlinearFactorGraph.
+//在GTSAM中，量测函数表示为“因子”。库中提供了一些常见的因子来解决机器人技术/ SLAM /BA问题。
+//我们将一个简单的先验应用到旋转上。我们通过NonlinearFactorGraph中的`addPrior`便捷方法来实现。
+
+// When the factors are created, we will add them to a Factor Graph. As the factors we are using
+// are nonlinear factors, we will need a Nonlinear Factor Graph.
+//创建因子后，我们会将其添加到因子图中。由于我们使用的因子是非线性因子，因此我们将需要一个非线性因子图。
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+
+// The nonlinear solvers within GTSAM are iterative solvers, meaning they linearize the
+// nonlinear functions around an initial linearization point, then solve the linear system
+// to update the linearization point. This happens repeatedly until the solver converges
+// to a consistent set of variable values. This requires us to specify an initial guess
+// for each variable, held in a Values container.
+//GTSAM中的非线性求解器是迭代求解器，这意味着它们会在初始线性化点附近线性化非线性函数，
+//然后求解线性系统以更新线性化点。这会反复发生，直到求解器收敛到一组一致的变量值为止。
+//这要求我们为保存在“值”容器中的每个变量指定一个初始猜测。
+#include <gtsam/nonlinear/Values.h>
+
+// Finally, once all of the factors have been added to our factor graph, we will want to
+// solve/optimize to graph to find the best (Maximum A Posteriori) set of variable values.
+// GTSAM includes several nonlinear optimizers to perform this step. Here we will use the
+// standard Levenberg-Marquardt solver
+//最后，将所有因子添加到因子图中后，我们将需要求解/优化以找到最佳变量值集（最大后验）。
+//GTSAM包含多个非线性优化器来执行此步骤。在这里，我们将使用标准的Levenberg-Marquardt求解器。
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+
+
+using namespace std;
+using namespace gtsam;
+
+const double degree = M_PI / 180;
+
+int main() {
+
+  /**
+   *    Step 1: Create a factor to express a unary constraint
+   * The "prior" in this case is the measurement from a sensor,
+   * with a model of the noise on the measurement.
+   步骤1：创建一个表达一元约束的因子
+   在这个例子中，“先验”是来自传感器的测量，并带有测量噪声的模型。
+   *
+   * The "Key" created here is a label used to associate parts of the
+   * state (stored in "RotValues") with particular factors.  They require
+   * an index to allow for lookup, and should be unique.
+   此处创建的“Key”是用于将状态的某些部分（存储在“ RotValues”中）与特定因素相关联的标签。
+   它们需要一个索引以允许查找，并且应该是唯一的。
+   *
+   * In general, creating a factor requires:
+   *  - A key or set of keys labeling the variables that are acted upon
+   *  - A measurement value
+   *  - A measurement model with the correct dimensionality for the factor
+   通常，创建一个因子需要：
+    -标记要作用的变量的一个key或一组key
+    -一个测量值
+    -具有正确维度的测量模型
+   */
+  Rot2 prior = Rot2::fromAngle(30 * degree);
+  prior.print("goal angle");
+  noiseModel::Isotropic::shared_ptr model = noiseModel::Isotropic::Sigma(1, 1 * degree);//各项同性噪声
+  Symbol key('x',1);
+
+  /**
+   *    Step 2: Create a graph container and add the factor to it
+   * Before optimizing, all factors need to be added to a Graph container,
+   * which provides the necessary top-level functionality for defining a
+   * system of constraints.
+   步骤2：创建一个graph容器并将因子添加到其中
+  优化之前，所有因素都需要添加到Graph容器中，该容器提供了用于定义约束系统的必要顶层功能。
+   *
+   * In this case, there is only one factor, but in a practical scenario,
+   * many more factors would be added.
+   在本例中，只有一个因子，但是在实际情况下，将添加更多因子。
+   */
+  NonlinearFactorGraph graph; //定义因子图
+  //PriorFactor<Rot2> factor(key, prior, model);
+  graph.addPrior(key, prior, model); //为节点x1添加先验边prior，model为噪声
+  graph.print("full graph");
+
+  /**
+   *    Step 3: Create an initial estimate
+   * An initial estimate of the solution for the system is necessary to
+   * start optimization.  This system state is the "RotValues" structure,
+   * which is similar in structure to a STL map, in that it maps
+   * keys (the label created in step 1) to specific values.
+   步骤3：建立初始估计
+  要开始优化，必须对系统的解进行初步估算。此系统的状态是“ RotValues”结构，
+  该结构与STL映射的结构相似，因为它会将key（在步骤1中创建的标签）映射到特定值。
+   *
+   * The initial estimate provided to optimization will be used as
+   * a linearization point for optimization, so it is important that
+   * all of the variables in the graph have a corresponding value in
+   * this structure.
+   提供给优化的初始估计值将用作优化的线性化点，因此，重要的是，图中的所有变量在此结构中均具有对应的值。
+   *
+   * The interface to all RotValues types is the same, it only depends
+   * on the type of key used to find the appropriate value map if there
+   * are multiple types of variables.
+   所有RotValues类型的接口都是相同的，如果存在多种类型的变量，则仅取决于用于查找适当的值的映射的key的类型。
+   */
+  Values initial;
+  initial.insert(key, Rot2::fromAngle(20 * degree));//赋初值
+  initial.print("initial estimate");
+
+  /**
+   *    Step 4: Optimize
+   * After formulating the problem with a graph of constraints
+   * and an initial estimate, executing optimization is as simple
+   * as calling a general optimization function with the graph and
+   * initial estimate.  This will yield a new RotValues structure
+   * with the final state of the optimization.
+   第4步：优化
+   在用约束图和初始估计表述问题之后，执行优化就是使用图和初始估计调用通用优化函数。
+   这将产生具有优化最终状态的新的RotValues。
+   */
+  Values result = LevenbergMarquardtOptimizer(graph, initial).optimize();
+  result.print("final result");
+
+  return 0;
+}
